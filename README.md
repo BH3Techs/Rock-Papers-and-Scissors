bd3f8b50a5788b262b274856889da7acd7f8bbdd
Howard Mabhugu <hmabhugu.3@gmail.com>
2022-12-15 11:25:24 +0200

    Computer chose Rock

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Rock-Papers-and-Scissors
 
+## The Visualized Rules of the Game
+
+![Game_Rules](./images/rps.png)
+
+## The Game Scenarios and Winners
+
+1. Rock vs Scissors -> Rock
+2. Rock vs Paper -> Paper
+3. Paper vs Rock -> Paper
+4. Paper vs Scissors -> Scissors
+5. Scissors vs Rock -> Rock
+6. Scissors vs Paper -> Scissors
--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
+const choice = ["ROCK", "PAPER", "SCISSOR"];
 function computerChoice(){
-    const choice = ["ROCK", "PAPER", "SCISSOR"];
     guessNumber = Math.floor(Math.random()*3);
     console.log(choice[guessNumber]);
     return choice[guessNumber];
 }
 function playRound(playerSelection, computerSelection){
     if(playerSelection==computerSelection){
+        //Computer and Player as the same choice
         return `Tie on ${playerSelection}`;
-    }else{
-        return 'Not yet known';
+    }else if(computerSelection == choice[0] && playerSelection == choice[2]){
+        //Computer chose ROCK
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }else if(computerSelection == choice[0] && playerSelection == choice[1]){
+        //Computer chose PAPER
+        return `You lose`
     }
 }
 console.log(playRound("ROCK",computerChoice));
