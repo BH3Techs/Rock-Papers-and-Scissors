174a050fb2263a011bf9ee548ca4bc466abbf975
Howard Mabhugu <hmabhugu.3@gmail.com>
2022-12-15 11:48:09 +0200

    All ROCK cases

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
 const choice = ["ROCK", "PAPER", "SCISSOR"];
+let player;
+let comp;
 function computerChoice(){
     guessNumber = Math.floor(Math.random()*3);
     console.log(choice[guessNumber]);
     return choice[guessNumber];
 }
 function playRound(playerSelection, computerSelection){
+    //Computer choice
     if(playerSelection==computerSelection){
         //Computer and Player as the same choice
         return `Tie on ${playerSelection}`;
-    }else if(computerSelection == choice[0] && playerSelection == choice[2]){
-        //Computer chose ROCK
+        //Rock choice
+    }else if(computerSelection == choice[0] || playerSelection == choice[0]){
+        if(computerSelection == "ROCK"){
+            switch(playerSelection){
+                case choice[1]:
+                    return `You Won! ${playerSelection} beat ${computerSelection}.`;
+                case choice[2]:
+                    return `You lose! ${computerSelection} beat ${playerSelection}.`;
+            }
+        }else{
+            switch(computerSelection){
+                case choice[1]:
+                    return `You Lose! ${computerSelection} beat ${playerSelection}.`;
+                case choice[2]:
+                    return `You Won! ${playerSelection} beat ${computerSelection}.`;
+            }
+        }
+        
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }else if(computerSelection == choice[0] && playerSelection == choice[1]){
-        //Computer chose PAPER
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
     }
 }
 console.log(playRound("ROCK",computerChoice));
